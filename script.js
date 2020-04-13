f595d4627b24312568aece244c15f193d9ced90a
Abisola-Morohunfolu <amorohunfolu@gmail.com>
2020-04-13 01:39:55 +0100

    added-observer

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const navItem = Array.from(document.querySelectorAll('.nav-list li'));
 const navBox = document.querySelector('.nav__box');
 const navLinks = document.querySelector('.nav-links');
 const navBrand = document.querySelector('.nav-brand');
+const works = document.querySelectorAll('.featured__work');
+const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
+const headingSix = document.querySelector('.works .heading-6');
 
 let menuTL = gsap.timeline({ paused: true, reversed: true });
 
@@ -51,4 +55,34 @@ menuTL
 
 hamburger.addEventListener('click', () => {
 	menuTL.reversed() ? menuTL.play() : menuTL.reverse();
+	if (hamburger.classList.contains('invert')) {
+		[lineOne, lineTwo].forEach((item) => item.classList.add('black-fill'));
+	} else {
+		[lineOne, lineTwo].forEach((item) => item.classList.remove('black-fill'));
+	}
 });
+
+const options = {
+	threshold: 0.5,
+};
+
+const navHeaderArr = [nav, header];
+
+const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+	entries.forEach((entry) => {
+		if (!entry.isIntersecting) {
+			navHeaderArr.forEach((item) => item.classList.remove('black'));
+
+			[navBrand, hamburger].forEach((item) => item.classList.remove('invert'));
+		} else {
+			navHeaderArr.forEach((item) => item.classList.add('black'));
+
+			[navBrand, hamburger].forEach((item) => item.classList.add('invert'));
+			entry.target.classList.add('appear');
+			// appearOnScroll.unobserve(entry.target);
+		}
+	});
+}, options);
+
+works.forEach((work) => appearOnScroll.observe(work));
+appearOnScroll.observe(headingSix);
